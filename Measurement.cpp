2742f0ad749717335ad85084048572c58de6bc69
Schwarz, Mario <7024d69eec63ad15@mytum.de>
2019-06-17 18:15:43 +0200

    Added abstract base class for measurements

---
+++ Measurement.cpp
@@ -0,0 +1,24 @@
+
+
+/*
+ * Autor: M Schwarz
+ * Mon 17 Jun 18:13:06 CEST 2019
+ *
+ *
+ *
+ *
+ */
+
+#include "Measurement.h"
+
+Measurement::Measurement(){
+
+}
+
+Measurement::~Measurement(){
+
+}
+
+
+
+
